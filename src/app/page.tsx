3a6f514e57da81637c59b0dae7d21e76025994c1
Dillon <179108267+iceorcus@users.noreply.github.com>
2025-04-25 00:34:55 +1000

    chore (fix): corrected issue with login page in light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import Head from "next/head";
 import Image from "next/image";
 import { SignIn } from "@clerk/nextjs"; 
+import { dark } from "@clerk/themes";
+import { Geist } from "next/font/google";
+
+const geistSans = Geist({
+  subsets: ["latin"],
+});
+
 
 export default function Home() {
-
   return (
-    <div className="bg-white w-full h-screen flex flex-col items-center justify-center">
-      <SignIn routing="hash" afterSignInUrl={"/:d:/app"} />
+    <div className={`${geistSans.className} bg-black w-full h-screen flex flex-col items-center justify-center`}>
+      <SignIn appearance={{
+        baseTheme: dark
+      }} routing="hash" afterSignInUrl={"/:d:/app"} />
     </div>
   );
 }
