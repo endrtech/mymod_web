c42617aead2b6a38e50263d192e4e5a68de00d73
Dillon <179108267+iceorcus@users.noreply.github.com>
2025-04-27 21:28:15 +1000

    fixed infinite reload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,14 @@
-import Head from "next/head";
-import Image from "next/image";
-import { dark } from "@clerk/themes";
-import { Geist, Space_Grotesk } from "next/font/google";
-import * as Clerk from "@clerk/elements/common";
-import * as SignIn from "@clerk/elements/sign-in";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import {
-  ChevronLeft,
-  Link,
-  Loader2,
-  MailCheck,
-  TextCursorInput,
-} from "lucide-react";
-import { cn } from "@/lib/utils";
-import { AnimatePresence } from "framer-motion";
-import { motion } from "framer-motion";
-import { Separator } from "@/components/ui/separator";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { getDiscordUser } from "@/app/actions/getDiscordUser"; 
-
-const geistSans = Space_Grotesk({
-  subsets: ["latin"],
-});
+import { getDiscordUser } from "@/app/actions/getDiscordUser";
 
 export default async function Home() {
-  const user = auth();
-  const router = useRouter();
+  const user = await auth();
   const discordData = await getDiscordUser();
-  
-  if(user.userId && discordData.id) {
+
+  if (user.userId && discordData.id) {
     return redirect("/:d:/app");
-  } else if(user.userId && !discordData.id) {
+  } else if (user.userId && !discordData.id) {
     return redirect("/connect-discord");
   }
 
