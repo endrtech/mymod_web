d892a3e5d85360a3244fe345335ab951eac5fcc5
Dillon <179108267+iceorcus@users.noreply.github.com>
2025-04-29 12:13:39 +1000

    chore (fix): Fixed updater dialog to be a drawer

--- src/components/UpdaterDialog.tsx
+++ src/components/UpdaterDialog.tsx
@@ -19,6 +19,13 @@ import { Check, CloudUpload } from "lucide-react";
 import { useRouter } from "next/navigation";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerFooter,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./ui/drawer";
 
 export const UpdaterDialog = () => {
   const router = useRouter();
@@ -56,110 +63,50 @@ export const UpdaterDialog = () => {
 
   if (appBuild !== 400) {
     return (
-      <Dialog>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <DialogTrigger asChild>
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className={`dark ${appBuild.currentBuild > appBuild.previousBuild && appVersion !== appBuild.version ? "text-orange-500 hover:text-orange-500" : "text-green-500 hover:text-green-500"}`}
-                >
-                  {appBuild.currentBuild > appBuild.previousBuild &&
-                  appVersion !== appBuild.version ? (
-                    <CloudUpload />
-                  ) : (
-                    <Check />
-                  )}
-                </Button>
-              </DialogTrigger>
-            </TooltipTrigger>
-            <TooltipContent
-              side="bottom"
-              className="block md:hidden z-[99] shadow-xl"
-            >
-              {appBuild.currentBuild > appBuild.previousBuild &&
-              appVersion !== appBuild.version ? (
-                <div className="flex flex-col items-start gap-2 p-2">
-                  <span className="font-bold text-md">
-                    Update {appBuild.version} is ready! Click "Update" to update
-                    MYMOD.
-                  </span>
-                  <Button
-                    onClick={() => updateApp()}
-                    variant="outline"
-                    className="dark text-white"
-                  >
-                    <CloudUpload /> Update
-                  </Button>
-                </div>
-              ) : (
-                <span className="p-2 font-bold text-md text-left">
-                  You&apos;re all up to date!
-                  <br />
-                  Click me for changelogs.
-                </span>
-              )}
-            </TooltipContent>
-            <TooltipContent
-              side="right"
-              className="hidden md:block z-[99] shadow-xl"
-            >
-              {appBuild.currentBuild > appBuild.previousBuild &&
-              appVersion !== appBuild.version ? (
-                <div className="flex flex-col items-start gap-2 p-2">
-                  <span className="font-bold text-md">
-                    Update {appBuild.version} is ready! Click "Update" to update
-                    MYMOD.
-                  </span>
-                  <Button
-                    onClick={() => updateApp()}
-                    variant="outline"
-                    className="dark text-white"
-                  >
-                    <CloudUpload /> Update
-                  </Button>
-                </div>
-              ) : (
-                <span className="p-2 font-bold text-md text-left">
-                  You&apos;re all up to date!
-                  <br />
-                  Click me for changelogs.
-                </span>
-              )}
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <DialogContent className="dark text-white">
-          <DialogTitle>Whats new in MYMOD {appBuild.version}</DialogTitle>
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
+            className={`dark ${appBuild.currentBuild > appBuild.previousBuild && appVersion !== appBuild.version ? "text-orange-500 hover:text-orange-500" : "text-green-500 hover:text-green-500"}`}
+          >
+            {appBuild.currentBuild > appBuild.previousBuild &&
+            appVersion !== appBuild.version ? (
+              <CloudUpload />
+            ) : (
+              <Check />
+            )}
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent className="dark text-white p-4">
+          <DrawerTitle className="mt-4 mb-4">
+            Whats new in MYMOD {appBuild.version}
+          </DrawerTitle>
           <ReactMarkdown remarkPlugins={[remarkGfm]}>
             {appBuild.changelog}
           </ReactMarkdown>
-        </DialogContent>
-      </Dialog>
+          <DrawerFooter className="flex flex-row items-center justify-end">
+            <Button
+              onClick={() => updateApp()}
+              variant="outline"
+              className="dark text-white"
+            >
+              <CloudUpload /> Update
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     );
   } else {
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button variant="outline" size="icon" className="dark text-red-500">
-            <CloudUpload />
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent
-          side="bottom"
-          className="md:hidden sm:block border-1 border-black shadow-xl"
-        >
-          Unable to fetch latest app build from the server.
-        </TooltipContent>
-        <TooltipContent
-          side="right"
-          className="sm:hidden md:block border-1 border-black shadow-xl"
-        >
-          Unable to fetch latest app build from the server.
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>;
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button variant="outline" size="icon" className="dark text-red-500">
+          <CloudUpload />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent className="w-full p-4">
+        Unable to fetch latest app build from the server.
+      </DrawerContent>
+    </Drawer>;
   }
 };
