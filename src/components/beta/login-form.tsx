0bb8f9c3fe07f4451f3e5a26f6f071c9b8704fd7
asink <d.veit0@proton.me>
2025-08-25 20:00:31 +1000

    Update login-form.tsx

--- src/components/beta/login-form.tsx
+++ src/components/beta/login-form.tsx
@@ -35,11 +35,11 @@ export function LoginForm({
           <form>
             <div className="grid gap-6">
               <div>
-                <Button variant="outline" className="w-full">
-                  <SignInButton mode="redirect" forceRedirectUrl="/portal">
+                <SignInButton mode="redirect" forceRedirectUrl="/portal">
+                  <Button variant="outline" className="w-full">
                     Login with ENDR ID
-                  </SignInButton>
-                </Button>
+                  </Button>
+                </SignInButton>
               </div>
             </div>
           </form>
