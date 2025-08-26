05f1a5c6c53cf2b9baf8970b844547c92b0ef5eb
onlyasink <d.veit0@proton.me>
2025-08-26 14:31:25 +1000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -24,27 +24,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://mymod.endr.my",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
   async rewrites() {
     return [
       {
